106e28eb503cf41ddb78050ad73c6d536064d73b
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2025-02-13 12:58:45 +0000

    Implement CreateProductHandler

--- src/Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -9,9 +10,21 @@ public record CreateProductResult(Guid Id);
 
 public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // business logic
-        throw new NotImplementedException();
+        // create product entity form command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        
+        // save product entity to database
+        
+        // return result with product id
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
