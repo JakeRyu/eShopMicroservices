8ecbc0bc24825f209bb336b4fbb698480662fef1
Jake Ryu <jake.ryu@outlook.com>
2025-02-15 11:32:37 +0000

    Add exception handler

--- src/Services/Catalog/Program.cs
+++ src/Services/Catalog/Program.cs
@@ -1,27 +1,53 @@
+using System.Net.Mime;
 using BuildingBlocks.Behaviours;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to DI container
 var assembly = typeof(Program).Assembly;
-builder.Services.AddMediatR(config =>
+builder.Services.AddMediatR(exceptionHandlerApp =>
 {
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    exceptionHandlerApp.RegisterServicesFromAssembly(assembly);
+    exceptionHandlerApp.AddOpenBehavior(typeof(ValidationBehaviour<,>));
 });
 
 builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddCarter();
 
-builder.Services.AddMarten(option =>
-{
-    option.Connection(builder.Configuration.GetConnectionString("Database")!);
-}).UseLightweightSessions();
+builder.Services.AddMarten(option => { option.Connection(builder.Configuration.GetConnectionString("Database")!); })
+    .UseLightweightSessions();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+            return;
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
