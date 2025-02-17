9544caedc5bdb70625a48959876c66859b8d2fcf
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2025-02-17 11:44:11 +0000

    Add GetBasket feature

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        // dodo: get basket form database
+
+        return new GetBasketResult(new ShoppingCart("new"));
+    }
+}
