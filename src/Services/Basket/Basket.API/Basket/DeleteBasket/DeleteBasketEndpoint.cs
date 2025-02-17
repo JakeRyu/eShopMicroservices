28234be66c754e366255522f4c99f118f38fba70
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2025-02-17 12:13:25 +0000

    Add DeleteBasket feature

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,27 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoint
+// public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/baskets/{userName}", async (string userName, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = new DeleteBasketCommand(userName);
+            
+            var result = await sender.Send(command, cancellationToken);
+
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteBasket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete a basket")
+        .WithDescription("Delete a basket by user name");
+    }
 }
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,25 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
-    
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty();
+    }
+}
+
+public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // todo: delete basket from database
+        
+        // todo: update cache
+
+        return new DeleteBasketResult(true);
+    }
 }
