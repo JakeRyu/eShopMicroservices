c736910552de580af41488ecef49004ca93bcf35
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2025-02-17 12:21:39 +0000

    Configure to use MediatR and Carter

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.RegisterServicesFromAssembly(assembly);
+    exceptionHandlerApp.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    exceptionHandlerApp.AddOpenBehavior(typeof(LoggingBehaviours<,>));
+});
+
+builder.Services.AddCarter();
 
 // --------------------------------------
 var app = builder.Build();
@@ -9,5 +18,6 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 // Configure the HTTP request pipeline
-
+app.MapCarter();
+ 
 app.Run();
