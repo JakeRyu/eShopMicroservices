a8eb8238ceedca5cbc960217c9f0d98952128029
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2025-02-17 12:01:00 +0000

    Add StoreBasket feature

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,27 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoint
+public record StoreBasketRequest(ShoppingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/baskets", async (StoreBasketResult request, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+            
+            var result = await sender.Send(command, cancellationToken);
+            
+            var response = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/baskets/{response.UserName}", response);
+        })
+        .WithName("Store basket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Store basket")
+        .WithDescription("Store basket in database");
+    }
 }
