79a2a36e62d11b90033c59f6ed45c722e54cc585
Jake Ryu <jake.ryu@outlook.com>
2025-03-23 21:51:01 +0000

    Handle BasketCheckoutEvent and publish OrderCreaedEvent

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,12 +1,15 @@
 using System.Reflection;
 using BuildingBlocks.Behaviours;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddMediatR(config =>
         {
@@ -14,6 +17,9 @@ public static class DependencyInjection
             config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
             config.AddOpenBehavior(typeof(LoggingBehaviours<,>));
         });
+
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly() );
+        
         return services;
     }
 }
