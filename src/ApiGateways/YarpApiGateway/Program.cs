e427a1d84c2a217493cef9aa14b034ec1d87aee7
Jake Ryu <jake.ryu@outlook.com>
2025-03-30 22:44:28 +0100

    YARP configuration

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure hte HTTP request pipeline
+app.MapReverseProxy();
 
 app.Run();
