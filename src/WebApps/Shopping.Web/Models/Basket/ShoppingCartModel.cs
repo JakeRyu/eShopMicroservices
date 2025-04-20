5f0984f527cae11690287353759c1d3b9b7a5d57
Jake Ryu <jake.ryu@outlook.com>
2025-04-20 21:21:10 +0100

    Index page

--- src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -10,9 +10,9 @@ public class ShoppingCartModel
 public class ShoppingCartItemModel
 {
     public int Quantity { get; set; } = default!;
-    public string Colour { get; set; } = default!;
+    public string Color { get; set; } = default!;
     public decimal Price { get; set; } = default!;
-    public string ProductId { get; set; } = default!;
+    public Guid ProductId { get; set; } = default!;
     public string ProductName { get; set; } = default!;
 }
 
